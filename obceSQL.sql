0f426c8415d52c943ac43696ca71312ae6d1845e
ktocek <ktocek@gmail.com>
2022-08-02 13:59:09 +0200

    final commit

--- obceSQL.sql
+++ obceSQL.sql
@@ -1,9 +1,14 @@
 -- 1
-select max(count) from(
+select nazov, count from(
 select o.nazov, count(o.nazov) as count from obec o
 group by o.nazov
 having count(o.nazov) > 1
-order by count desc) as nc;
+order by count desc) as nc
+where count = (select max(count) from(
+               select o.nazov, count(o.nazov) as count from obec o
+               group by o.nazov
+               having count(o.nazov) > 1
+               order by count desc) as nc);
 -- 2
 select count(*) from okres
 inner join kraj k on k.id = okres.id_kraj
@@ -20,22 +25,26 @@ where p.rok = 2012 and p.zeny + p.muzi = (select max(popu) from(
 select p.zeny + p.muzi as popu from populacia p
 inner join obec o on o.id = p.id_obec
 where p.rok = '2012') as np);
-
 -- 5
 select sum(p.muzi + p.zeny) as populacia from okres o
 inner join obec o2 on o.id = o2.id_okres
 inner join populacia p on o2.id = p.id_obec
 where o.nazov = 'Sabinov' and p.rok = 2012;
 -- 6
-select rok, sum(muzi + zeny) as popu from populacia
+select rok, sum(muzi + zeny) as populacia from populacia
 group by rok
 order by rok desc;
 -- 7
-select o.nazov, p.zeny + p.muzi as popu from populacia p
+select o.nazov, p.zeny + p.muzi as populacia from populacia p
 inner join obec o on o.id = p.id_obec
 inner join okres o2 on o2.id = o.id_okres
-where p.rok = '2011' and o2.nazov = 'Tvrdosin'
-order by popu;
+where p.rok = '2011' and o2.nazov = 'Tvrdosin' and p.zeny + p.muzi =
+            (select min(p.zeny + p.muzi) as populacia from populacia p
+             inner join obec o on o.id = p.id_obec
+             inner join okres o2 on o2.id = o.id_okres
+             where p.rok = '2011' and o2.nazov = 'Tvrdosin'
+            )
+order by populacia;
 -- 8
 select o.nazov from obec o
 inner join populacia p on o.id = p.id_obec
@@ -47,12 +56,11 @@ where p.zeny + p.muzi > 20000 and p.rok = 2011 and p.zeny > p.muzi
 order by pomer desc
 limit 10;
 -- 10
-select k.nazov, count(o.nazov) as okres, count(o2.nazov) as obec, sum(p.zeny + p.muzi) from kraj k
+select k.nazov as kraj, count(o.nazov) as okres, count(o2.nazov) as obec, sum(p.zeny + p.muzi) as populacia from kraj k
 inner join okres o on k.id = o.id_kraj
 inner join obec o2 on o.id = o2.id_okres
 inner join populacia p on o2.id = p.id_obec and p.rok = '2012'
 group by k.nazov;
-
 -- 11
 select nazov, rok12 as r2012, rok11 as r2011, rok12 - rok11 as rozdiel from(
 select o.nazov, sum(case when p.rok = 2011 then p.zeny + p.muzi end) as rok11,
@@ -66,6 +74,3 @@ select o.nazov from obec o
 inner join populacia p on o.id = p.id_obec
 where p.rok = 2012 and p.muzi + p.zeny < ( select avg(muzi + zeny) from populacia
 where rok = '2012');
-
-
-
